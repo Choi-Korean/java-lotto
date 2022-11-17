c965b3f8246fe9d7d01d35d365dde147d3b29524
cho2_mac <chso1324@naver.com>
2022-11-17 17:17:13 +0900

    feat: 로또구매금액 입력 기능

--- src/main/java/lotto/view/Front.java
+++ src/main/java/lotto/view/Front.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Front {
 
-
+    public String buyLotto(){
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
 
 }
